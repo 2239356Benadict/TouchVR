b94e4fd7a8088c4f365190964d8f5d302e10e6f2
KARTHICK JAYACHANDRAN <2215211@Swansea.ac.uk>
2022-10-19 00:39:24 +0100

    AssignmentChange

---
+++ Assets/Scripts/Assignment.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Master The Blaster");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
