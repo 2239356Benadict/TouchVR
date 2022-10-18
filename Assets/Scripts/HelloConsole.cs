ee3a2340aa9a22a3b55c81ae36ab55c8f98ce15a
KARTHICK JAYACHANDRAN <2215211@Swansea.ac.uk>
2022-10-19 00:58:01 +0100

    Step15

--- Assets/Scripts/HelloConsole.cs
+++ Assets/Scripts/HelloConsole.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class HelloConsole : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Write to Console
     void Start()
     {
-        Debug.Log("Benadict Joseph");
+        Debug.Log("2215211");
     }
 
     // Update is called once per frame
